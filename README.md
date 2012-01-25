ff2a4ad921b08b04003ba526f4664452fe8f5efd
Scott Muc <scottmuc@gmail.com>
2012-01-25 09:06:16 -0800

    Organized Files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## My Dot Files
+
+To install:
+
+* Clone to ~/.dotfiles
+* `sh ~/.dotfiles/install.sh
+
---
+++ install.sh
@@ -0,0 +1,6 @@
+
+ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
+ln -s ~/.dotfiles/git/gitignore ~/.gitignore
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
