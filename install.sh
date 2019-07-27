82d9bf111258220c0c3ee75fe9bf053ae2e4f65b
Nishitha Ningegowda <nningego@thoughtworks.com>
2019-07-27 13:47:25 +0100

    fishfile and fish variables

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 
-ln -s ~/.dotfiles/bash/git-completion.sh ~/.git-completion.sh
-ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -fs ~/.dotfiles/bash/git-completion.sh ~/.git-completion.sh
+ln -fs ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -fs ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
-ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/githelpers ~/.githelpers
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -fs ~/.dotfiles/git/gitignore ~/.gitignore
+ln -fs ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -fs ~/.dotfiles/git/githelpers ~/.githelpers
+ln -fs ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -fs ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -fs ~/.dotfiles/fish/fishfile ~/.config/fish/fishfile
+ln -fs ~/.dotfiles/fish/fish_variables ~/.config/fish/fish_variables
